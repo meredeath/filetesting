de1ce7b8b3b4f2fe8370c4a3096f1fe03d9fa2aa
Meredith <mhu2@stuy.edu>
2018-10-23 14:47:30 -0400

    open and close work now!

--- main.c
+++ main.c
@@ -4,23 +4,38 @@
 #include<sys/stat.h>
 #include <fcntl.h>
 
-/*
 int main(){
-  int fc = open("/tmp/test.txt", O_RDWR);
 
-  printf("fc: %d\n", fc);
-  
-  if(fc == -1){
-    printf("It didn't work!");
+  printf("Testing open:\n");
+  int f = open("tmp/test.txt", O_RDWR);
+  //printf("f: %d\n", f);
+  if(f == -1){
+    printf("It didn't work! Wrong path, maybe? your value is %d\n",f);
+  }else{
+    printf("you successfully opened the file! your value is %d\n",f);
   }
 
-  close(fc);
+  printf("\nTesting read:\n");
+  //put stuff here
+
+  printf("\nTesting write:\n");
+  //put stuff here
+  
+  printf("\nTesting close:\n");
+  int i = close(f);
+  if(i == 0){
+    printf("You successfully closed the file! your value is %d\n",i);
+  }else{
+    printf("You didn't close the file :( your value is %d\n",i);
+  }
+  //printf("i: %d\n", i);
   return 0;
 }
-*/
+
+/*
 int main() 
 { 
-    int fd1 = open("foo.txt", O_RDONLY); 
+    int fd1 = open("/tmp/test.txt", O_RDONLY); 
     if (fd1 < 0)  
     { 
         perror("c1"); 
@@ -36,3 +51,4 @@ int main()
     }  
     printf("closed the fd.\n"); 
 } 
+*/
