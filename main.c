799967ce7e970fa4b1494d72ea21cc482f5f42d9
tfabiha <tfabiha@stuy.edu>
2018-10-23 22:07:09 -0400

    finished all four functions

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<sys/types.h>
-#include<sys/stat.h>
+#include <string.h>
+#include <unistd.h>
 #include <fcntl.h>
 
 int main(){
@@ -16,11 +16,30 @@ int main(){
   }
 
   printf("\nTesting read:\n");
-  //put stuff here
+  if (f == -1)
+  {
+    printf("You can't access the file to read it\n");
+  }
+  else
+  {
+    char * str = (char *) malloc(100 * sizeof(char));
+    int len = read(f, str, 10);
+    str[len] = '\0';
+
+    printf("Reading %d bytes from file: %s\n", len, str);
+  }
 
   printf("\nTesting write:\n");
-  //put stuff here
-  
+  if (f == -1)
+  {
+    printf("You can't access the file to write to it\n");
+  }
+  else
+  {
+    int lenWritten = write(f, "what's up\n", 100);
+    printf("Wrote \'what's up to file. Number of bytes written: %d\'\n", lenWritten);
+  }
+
   printf("\nTesting close:\n");
   int i = close(f);
   if(i == 0){
@@ -33,22 +52,22 @@ int main(){
 }
 
 /*
-int main() 
-{ 
-    int fd1 = open("/tmp/test.txt", O_RDONLY); 
-    if (fd1 < 0)  
-    { 
-        perror("c1"); 
-        exit(1); 
-    } 
-    printf("opened the fd = % d\n", fd1); 
-      
-    // Using close system Call 
-    if (close(fd1) < 0)  
-    { 
-        perror("c1"); 
-        exit(1); 
-    }  
-    printf("closed the fd.\n"); 
-} 
+int main()
+{
+    int fd1 = open("/tmp/test.txt", O_RDONLY);
+    if (fd1 < 0)
+    {
+        perror("c1");
+        exit(1);
+    }
+    printf("opened the fd = % d\n", fd1);
+
+    // Using close system Call
+    if (close(fd1) < 0)
+    {
+        perror("c1");
+        exit(1);
+    }
+    printf("closed the fd.\n");
+}
 */
